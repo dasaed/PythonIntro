8afc270f347ff57873e43a055454b8814b987182
dasaed <carlos.dasaed@gmail.com>
2019-05-01 21:51:20 -0230

    last change of the night

---
+++ app.py
@@ -0,0 +1,11 @@
+print("#####################################################")
+print("Python side-notes")
+print("*The order in which you write the instructions matters")
+print("*Don't mix tabs and spaces; they are not interchangeable")
+print("#####################################################")
+print("Variables in Python")
+age=70
+name="Carlos"
+print(""+str(age)+" "+3*name)
+print("")
+print("")
