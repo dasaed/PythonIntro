ca104daaa464753bc90c33c56e3ca4805837e94c
dasaed <carlos.dasaed@gmail.com>
2019-05-10 21:18:54 -0230

    extra files

---
+++ useful_tools.py
@@ -0,0 +1,12 @@
+import random
+
+feet_in_mile = 5280
+meters_in_km = 1000
+
+proteams = ["Dallas Cowboys", "Miami Dolphins", "New England Patriots", "Greenbay Packers"]
+
+def get_file_ext(filename):
+    return filename[filename.index(".")+1:]
+
+def roll_dice(num):
+    return random.randint(1,num)
